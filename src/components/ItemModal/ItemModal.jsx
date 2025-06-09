6da28e21ad85929d2abc645e4cab7480a9848592
joshua zimmerman <zimmerman.joshua@gmail.com>
2025-06-09 13:21:33 -0500

    modal updated

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -3,30 +3,15 @@ import logger from "../../utils/logger.jsx";
 import { useContext } from "react";
 import { CurrentUserContext } from "../../contexts/CurrentUserContext.jsx";
 
-const ItemModal = ({ selectedCard, onClose, onCardDelete, isLoggedIn, onCardLike }) => {
+const ItemModal = ({ selectedCard, onClose, onCardDelete, isLoggedIn }) => {
   logger("ItemModal");
 
   const currentUser = useContext(CurrentUserContext);
 
   // current user is the owner of the item
-  // More forgiving check
   const isOwn = selectedCard.owner === currentUser?._id || !selectedCard.owner;
-
   const canDelete = isLoggedIn && isOwn;
 
-  // delete button visibility
-  const deleteButtonClassName = `delete__button ${canDelete ? "delete__button_visible" : "delete__button_hidden"}`;
-
-
-  const isLiked = selectedCard.likes && currentUser
-    ? selectedCard.likes.some(id => id === currentUser._id)
-    : false;
-
-  // like button click
-  const handleLikeClick = (e) => {
-    e.stopPropagation(); // Prevent modal from closing
-    onCardLike({ id: selectedCard._id, isLiked: !isLiked });
-  };
   return (
     <div className="modal">
       <div className="modal__content">
@@ -36,12 +21,7 @@ const ItemModal = ({ selectedCard, onClose, onCardDelete, isLoggedIn, onCardLike
           className="preview__close-button"
         ></button>
 
-        {/* Header with name and like button */}
-         <div className="modal__header">
-          <h3 className="modal__item-name">{selectedCard.name}</h3>
-        </div>
-
-        {/* Image container */}
+        {/* Image container - bigger in upper half */}
         <div className="modal__image-container">
           <img
             className="modal__image"
@@ -50,18 +30,19 @@ const ItemModal = ({ selectedCard, onClose, onCardDelete, isLoggedIn, onCardLike
           />
         </div>
 
+        {/* Bottom section with name, weather, and delete button */}
         <div className="modal__footer">
           <div className="modal__info">
-            <h3 className="modal__item-name">{selectedCard.name}</h3>
+            <p className="modal__item-name">{selectedCard.name}</p>
             <p className="modal__weather-type">Weather: {selectedCard.weather}</p>
           </div>
 
-          {isLoggedIn && isOwn && (
+          {canDelete && (
             <button
-              className={deleteButtonClassName}
+              className="delete__button"
               onClick={() => onCardDelete(selectedCard)}
             >
-              Delete Item
+              Delete item
             </button>
           )}
         </div>
@@ -69,4 +50,5 @@ const ItemModal = ({ selectedCard, onClose, onCardDelete, isLoggedIn, onCardLike
     </div>
   );
 };
+
 export default ItemModal;
